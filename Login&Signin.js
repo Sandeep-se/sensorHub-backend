13bd720a5ef150b8c4a4d45f987d4f5762d5ae15
Sandeep <sandeep.se2021ecec@sece.ac.in>
2023-09-07 19:31:29 +0530

    a

--- Login&Signin.js
+++ Login&Signin.js
@@ -33,7 +33,7 @@ const signIn=async(req,res)=>
             {
                 res.cookie('userId', check._id.toString(), {
                     maxAge: 900000, 
-                    path:'/',
+                    path:'https://amazon-clone-frontend-eta.vercel.app/',
                     httpOnly: true, 
                     secure:true,
                     sameSite:'none'
